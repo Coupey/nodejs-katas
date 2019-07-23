5aab03e4bcb0f826f15235a9d0907d277c20311b
Paul Coupe <paul.coupe@digital.homeoffice.gov.uk>
2019-07-23 11:37:55 +0100

    bowling update

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,2 +1,54 @@
-// Enjoy arrow functions
-export default () => 'Hello World!';
+const ballsPerFrame = 2;
+
+class BowlingGame {
+  constructor() {
+    this.runningScore = 0;
+    this.frameScore = 0;
+    this.frameBallNumber = 1;
+    this.numberOfSpares = 0;
+  }
+
+  activateSpares(sparesToAdd) {
+    this.numberOfSpares += sparesToAdd;
+  };
+
+  endFrame() {
+    this.frameBallNumber = 1;
+    this.frameScore = 0;
+  };
+
+  roll(pins) {
+    this.runningScore += pins;
+    this.frameScore += pins;
+
+    // add bonus
+    if (this.numberOfSpares > 0){
+      this.runningScore += pins;
+      this.numberOfSpares--;
+    }
+
+    // check for strikes and spares
+    if (this.frameScore === 10) {
+      if (this.frameBallNumber === 1){
+        this.activateSpares(2);
+      } else {
+        this.activateSpares(1);
+      }
+
+      this.endFrame();
+    } else {
+      this.frameBallNumber++;
+      if (this.frameBallNumber > ballsPerFrame){
+        this.endFrame();
+      }
+    }
+
+  }
+
+  score() {
+    return this.runningScore;
+  }
+
+}
+
+module.exports = BowlingGame;
--- test/HelloWorld.test.js
+++ test/HelloWorld.test.js
@@ -1,9 +1,60 @@
 // import default from modules
-import HelloWorld from '../src/HelloWorld';
+let BowlingGame = require("../src/BowlingGame");
+let expect = require('chai').expect;
 
-// Write ES6 mocha tests with Chai assertions
-describe('Hello World', () => {
-  it('should return "Hello World!"', () => {
-    expect(HelloWorld()).to.equal('Hello World!');
+describe('Bowling game', function() {
+
+  let game;
+
+  beforeEach(function() {
+    game = new BowlingGame();
   });
+
+  // simple 1 roll
+  it('should return a score of 0 when roll with no pins hit', () => {
+    game.roll(0);
+    expect(game.score()).to.equal(0);
+  });
+
+
+  // simple 1 roll
+  it('should return a score of 5 when 5 pins hit', () => {
+    game.roll(5);
+    expect(game.score()).to.equal(5);
+  });
+
+  // 2 rolls
+  it('should return correct score from 2 rolls', () => {
+    game.roll(5);
+    game.roll(2);
+    expect(game.score()).to.equal(7);
+  });
+
+  // 3 rolls - no spare
+  it('should return correct score from 3 normal rolls', () => {
+    game.roll(5);
+    game.roll(2);
+    game.roll(2);
+    expect(game.score()).to.equal(9);
+  });
+
+
+  // 3 rolls - there's a spare
+  it('should return correct score from 3 rolls where first 2 are a spare', () => {
+    game.roll(5);
+    game.roll(5);
+    game.roll(5);
+    expect(game.score()).to.equal(20);
+  });
+
+  // 4 rolls - there's a strike
+  it('should return correct score from 4 rolls where first roll is a strike', () => {
+    game.roll(10);
+    game.roll(3);
+    game.roll(5);
+    game.roll(5);
+    expect(game.score()).to.equal(31);
+  });
+
+
 });
